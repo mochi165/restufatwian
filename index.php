9b2778a3dc5776d0580d72a5ea22ae20f57b2831
Restu Fatwian <79190530+mochi165@users.noreply.github.com>
2022-09-11 15:39:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -229,7 +229,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="0.1s">
+            <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="">
                 <div class="col-lg-4 col-md-6 portfolio-item first">
                     <div class="portfolio-img rounded overflow-hidden">
                         <img class="img-fluid" src="img/1.png" alt="">
